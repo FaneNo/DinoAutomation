e070c8423a451b06a82050136730901b57df3413
FaneNo <102693139+FaneNo@users.noreply.github.com>
2023-09-07 23:34:51 -0700

    Complete
    Finished the everything

--- DinoAuto.py
+++ DinoAuto.py
@@ -1,49 +1,37 @@
+
 import time
 import pyautogui
-from PIL import ImageGrab
+from PIL import ImageGrab, ImageOps
 
-def click(key):
-    pyautogui.keyDown(key)
-    return
+#coordinate for the restart button
+restart = (1282, 778 )
 
-while True:  # for infinity loop
-    # capture image in black & white format
-    image = ImageGrab.grab().convert('L')   
-    data = image.load()
-    # Draw the rectangle for birds
-    for i in range(530, 560):
-        for j in range(100, 125):
-            data[i, j] = 171
+#the coordinate of the left side of the dino and the right side
+dino = (10, 795, 175, 980)   
+
+def image():
+    #find the box that only have the dino in it
+    box = (dino[0] + 350, dino[1], dino[2] + 350, dino[3] - 45)
+    img = ImageGrab.grab(box)
+    #convert the image to grayscale
+    grayScale = ImageOps.grayscale(img)
+    #get the color
+    b = grayScale.getcolors()
+    #sum the tuple number up from the getcolors function
+    a = sum(map(sum, b))
+    return a
       
-    for i in range(5, 500):
-        for j in range(130, 200):
-            data[i, j] = 0
-    image.show()
-    break
-                  
+def main():
+    pyautogui.click(restart)
+    while True:
+        #continously capture the state of the game
+        image()
+        #if the image sum is not value 23355 from the capture box jump
+        if(image() != 23355):
+            pyautogui.keyDown('space')
+            time.sleep(0.001)
+            pyautogui.keyUp('space')
+            time.sleep(0.001)
             
-
-
-# def isCollision(data):
-# # Check colison for birds
-#     for i in range(530,560):
-#         for j in range(80, 127):
-#             if data[i, j] < 171:
-#                 click("down")
-#                 return
-#  # Check colison for cactus
-#     for i in range(530, 620):
-#         for j in range(130, 160):
-#             if data[i, j] < 100:
-#                 click("up")
-#                 return
-#     return
-
-# if __name__ == "__main__":
-#     time.sleep(5)
-#     click('up') 
-    
-#     while True:
-#         image = ImageGrab.grab().convert('L')  
-#         data = image.load()
-#         isCollision(data)
+if __name__ == "__main__":
+    main()           
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DinoAutomation
  A script that will autmatically play the dino google game for you
+
+ # Limitation
+ Screen required to be 2560 x 1440 
+ If the monitor hz is above 60 it can cause program
